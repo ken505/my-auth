317b1ce3b0987d2c0a69d2c06f82218362250966
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-11 15:11:50 +0900

    ♻️ Change to Arrow function.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import { AuthUserProvider } from '../context/AuthUserContext';
+import "../styles/globals.css";
+import { AuthUserProvider } from "../context/AuthUserContext";
 
-function MyApp({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
+// function MyApp({ Component, pageProps }) {
+const MyApp = (props) => {
+  return (
+    <AuthUserProvider>
+      <props.Component {...props.pageProps} />
+    </AuthUserProvider>
+  );
+};
+export default MyApp;
+
+{
+  /* <AuthUserProvider>
+      <Component {...pageProps} />
+    </AuthUserProvider> */
 }
-
-export default MyApp
